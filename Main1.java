064a71da84d1c1e5960b6991dbe864e2f52d8160
Godwin <godwinmail6@gmail.com>
2023-10-10 21:49:21 +0200

    first question solved

--- Main1.java
+++ Main1.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class Main1 {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        Set<String> setWords = new HashSet<>();
+
+        try{
+
+            System.out.println("Add any number between 5 - 10");
+            int numFromUser = Integer.parseInt(input.nextLine());
+
+            ArrayList<String> duplicate = new ArrayList<>();
+            for (int i = 0; i < numFromUser; i++){
+                System.out.println("add new word: ");
+                String word = input.nextLine();
+                if (!setWords.add(word)){
+                    duplicate.add(word);
+                }
+            }
+           /* System.out.println(duplicate);
+            System.out.println(setWords.size());
+            System.out.println(setWords);*/
+
+
+        }catch(Exception e){
+            System.err.println("Incorrect input");
+        }
+
+
+
+
     }
+
 }
